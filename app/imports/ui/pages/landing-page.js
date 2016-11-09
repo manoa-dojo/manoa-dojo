93edebb32edbf3fc0d4a206fb3168f733bb23083
Ryan Barsatan <ryanbinfo@gmail.com>
2016-11-08 17:01:14 -1000

    Uh-Cas Login completed for landing page and header

--- app/imports/ui/pages/landing-page.js
+++ app/imports/ui/pages/landing-page.js
@@ -1,27 +1,33 @@
 import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
 
 Template.Landing_Page.onCreated(function onCreated() {
-	console.log('test01');
+	console.log(Meteor.userId());
+
 });
 
 Template.Landing_Page.helpers({
 });
 
 Template.Landing_Page.events({
-	'click #signup-btn'(event){
-		$('#action-btns').slideToggle();
-        $('#signup').slideToggle();
+	'click .cas-logout': function casLogout(event) {
+		event.preventDefault();
+		Meteor.logout();
+		return false;
 	},
-	'click #login-btn'(event){
-		$('#action-btns').slideToggle();
-        $('#login').slideToggle();
-	},
-	'click #signup-back'(event){
-      $('#signup').slideToggle();
-      $('#action-btns').slideToggle();
-	},
-	'click #login-back'(event){
-      $('#login').slideToggle();
-      $('#action-btns').slideToggle();
+
+	'click .cas-login': function casLogin(event, instance) {
+		event.preventDefault();
+		if(confirm("By logging in you agree to the terms and conditions stated in the about page."))
+		{
+		  const callback = function loginCallback(error) {
+		    if (error) {
+		      console.log(error);
+		    }
+		  };
+		  Meteor.loginWithCas(callback);
+		  return false;
+		}
+
 	},
 });
