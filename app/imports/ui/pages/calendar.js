0f05b6cccd9a367b74b43fd79d18847ac4c6c335
Ryan Barsatan <ryanbinfo@gmail.com>
2016-11-13 23:08:08 -1000

    Implemented Calendar & Fixed Nav Bar
    Calendar now reflects all available sections.
    Nav bar now uses logo with white outline and is properly sized.

--- app/imports/ui/pages/calendar.js
+++ app/imports/ui/pages/calendar.js
@@ -1,90 +1,38 @@
 import { Template } from 'meteor/templating';
+import { Sections } from '../../api/sections/sections.js';
+import { Meteor } from 'meteor/meteor';
 
 Template.Calendar_Page.onCreated(function onCreated() {
-
+	this.autorun(() => {
+		this.subscribe('Sections');
+	});
 
 });
 
 Template.Calendar_Page.onRendered(function() {
-	    $('#calendar').fullCalendar({
-            events: [
-		        {
-		            title  : 'ICS 314 Meeting',
-		            start  : '2016-10-01',
-		            color: 'orange',
-		        },
-		        {
-		            title  : 'Physics 212',
-		            start  : '2016-10-05',
-		            end    : '2016-10-07',
-		            color: 'green',
 
-		        },
-		        {
-		            title  : 'Psycology 200 Tutoring',
-		            start  : '2016-10-09T12:30:00',
-		            allDay : false, // will make the time show
-		            color: 'orange',
-
-		        },
-		        {
-		            title  : 'Physics 212',
-		            start  : '2016-10-10',
-		            end    : '2016-10-13',
-		            color: 'green',
-		            link: "google.com"
-		        },
-		        {
-		            title  : 'ICS 314 Meeting',
-		            start  : '2016-10-20',
-		            end    : '2016-10-20',
-		            color: 'orange',
-
-		        },
-		    ]
-    });
 
 });
 
 Template.Calendar_Page.helpers({
+
     options: function() {
-	    return {
-	        events: [
-		        {
-		            title  : 'ICS 314 Meeting',
-		            start  : '2016-11-01',
-		            color: 'orange',
-		        },
-		        {
-		            title  : 'Physics 212',
-		            start  : '2016-11-05',
-		            end    : '2016-11-07',
-		            color: 'green',
-
-		        },
-		        {
-		            title  : 'Psycology 200 Tutoring',
-		            start  : '2016-11-09T12:30:00',
-		            allDay : false, // will make the time show
-		            color: 'orange',
-
-		        },
-		        {
-		            title  : 'Physics 212',
-		            start  : '2016-11-10',
-		            end    : '2016-11-13',
-		            color: 'green',
-		            link: "google.com"
-		        },
-		        {
-		            title  : 'ICS 314 Meeting',
-		            start  : '2016-11-20',
-		            end    : '2016-11-20',
-		            color: 'orange',
-
-		        },
-		    ]
-	    };
+    	let sec = Sections.find().fetch();
+    	let sectionArr = [];
+        let eventColor = 'orange';
+    	for(section of sec){
+            if (Meteor.user().userName == section.createdBy){
+                eventColor = 'green';
+            };
+    		sectionArr.push({
+    			title: section.course,
+    			start: section.startTime.toLocaleString(),
+    			end: section.endTime.toLocaleString(),
+                url: '../study-section',
+    			color: eventColor,
+    		});
+    	};
+	    return {events: sectionArr};
     }
 });
 
